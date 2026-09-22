5c84466977bee94ba0baf6eaa01cd7378d143c22
Vishara Jayalath <visharajayalath04@gmail.com>
2026-09-23 00:12:11 +0530

    Update social links

--- src/consts.ts
+++ src/consts.ts
@@ -30,15 +30,11 @@ export const PROJECTS: Metadata = {
 
 export const SOCIALS: Socials = [
   { 
-    NAME: "twitter-x",
-    HREF: "https://twitter.com/markhorn_dev",
-  },
-  { 
     NAME: "github",
-    HREF: "https://github.com/markhorn-dev"
+    HREF: "https://github.com/visharaaa"
   },
   { 
     NAME: "linkedin",
-    HREF: "https://www.linkedin.com/in/markhorn-dev",
+    HREF: "https://www.linkedin.com/in/vishara-jayalath",
   }
 ];
