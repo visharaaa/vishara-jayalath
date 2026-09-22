837b08b8052f519c12116938a9cd39645ad62e9f
Vishara Jayalath <visharajayalath04@gmail.com>
2026-09-23 00:43:38 +0530

    Adding email

--- src/consts.ts
+++ src/consts.ts
@@ -1,8 +1,8 @@
 import type { Site, Metadata, Socials } from "@types";
 
 export const SITE: Site = {
-  NAME: "Astro Nano",
-  EMAIL: "markhorn.dev@gmail.com",
+  NAME: "Vishara Jayalath",
+  EMAIL: "visharajayalath04@gmail.com",
   NUM_POSTS_ON_HOMEPAGE: 3,
   NUM_WORKS_ON_HOMEPAGE: 2,
   NUM_PROJECTS_ON_HOMEPAGE: 3,
